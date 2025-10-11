e27e0c35ce8b99967eecd884f465e73e81d8afae
Brian Li <me@brian14708.dev>
2025-10-11 21:17:14 +0800

    feat: initial trace-ui

---
+++ trace-ui/src/App.svelte
@@ -0,0 +1,15 @@
+<svelte:options customElement={{ tag: "my-element" }} />
+
+<script lang="ts">
+  import "./app.css";
+  let { name = "world" } = $props();
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello {name}!</h1>
+<slot />
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
